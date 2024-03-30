1f9ac2f393c75a157b9810a615f19767bff4ca56
Alex Fischman <alexdfischman@gmail.com>
2024-03-30 10:26:30 -0700

    Fix player walking

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,11 +3,13 @@ extends CharacterBody3D
 @export var JUMP_DIST: float
 @export var JUMP_HEIGHT: float
 @export var JUMP_TIME: float
+@export var MOVE_ACCEL: float
 
 func _physics_process(delta: float) -> void:
 	var MOVE_SPEED: float = JUMP_DIST / JUMP_TIME
 	var GRAVITY_FORCE: float = 8 * JUMP_HEIGHT / JUMP_TIME / JUMP_TIME
 	var JUMP_IMPULSE: float = 4 * JUMP_HEIGHT / JUMP_TIME
+	var MOVE_FRICTION: float = MOVE_ACCEL / MOVE_SPEED
 
 	var vertical: float = velocity.y
 	velocity.y = 0
@@ -21,8 +23,8 @@ func _physics_process(delta: float) -> void:
 		vertical -= GRAVITY_FORCE * delta
 
 	var input: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var direction: Vector3 = self.transform.basis * Vector3(input.x, 0, input.y) * MOVE_SPEED
-	velocity = velocity.lerp(direction, 0.5)
+	var direction: Vector3 = self.transform.basis * Vector3(input.x, 0, input.y) * MOVE_ACCEL
+	velocity += (direction - velocity * MOVE_FRICTION) * delta
 
 	velocity.y = vertical
 	move_and_slide()
