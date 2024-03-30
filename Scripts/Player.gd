49aa3d7650b5b2ab7532550a5b7b31599a080405
Alex Fischman <alexdfischman@gmail.com>
2024-03-30 15:37:53 -0700

    Fix velocity and lower air movement

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,6 +4,7 @@ extends CharacterBody3D
 @export var JUMP_HEIGHT: float
 @export var JUMP_TIME: float
 @export var MOVE_ACCEL: float
+@export var AIR_MOVE_ACCEL: float
 
 @export var ROPE_CAST: ShapeCast3D
 var ROPE_WITH_ENDPOINTS: PackedScene = preload("res://Scenes/RopeWithEndpoints.tscn")
@@ -17,9 +18,10 @@ var rope_length: float
 
 func _physics_process(delta: float) -> void:
 	var MOVE_SPEED: float = JUMP_DIST / JUMP_TIME
+	var MOVE_FRICTION: float = MOVE_ACCEL / MOVE_SPEED
+	var AIR_MOVE_FRICTION: float = MOVE_SPEED / AIR_MOVE_ACCEL
 	var GRAVITY_FORCE: float = 8 * JUMP_HEIGHT / JUMP_TIME / JUMP_TIME
 	var JUMP_IMPULSE: float = 4 * JUMP_HEIGHT / JUMP_TIME
-	var MOVE_FRICTION: float = MOVE_ACCEL / MOVE_SPEED
 
 	var vertical: float = velocity.y
 	velocity.y = 0
@@ -33,8 +35,10 @@ func _physics_process(delta: float) -> void:
 		vertical -= GRAVITY_FORCE * delta
 
 	var input: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var direction: Vector3 = self.transform.basis * Vector3(input.x, 0, input.y) * MOVE_ACCEL
-	velocity += (direction - velocity * MOVE_FRICTION) * delta
+	var move_accel: float = MOVE_ACCEL if self.is_on_floor() else AIR_MOVE_ACCEL
+	var move_friction: float = MOVE_FRICTION if self.is_on_floor() else AIR_MOVE_FRICTION
+	var direction: Vector3 = self.transform.basis * Vector3(input.x, 0, input.y) * move_accel
+	velocity += (direction - velocity * move_friction) * delta
 
 	velocity.y = vertical
 	move_and_slide()
@@ -55,6 +59,9 @@ func _physics_process(delta: float) -> void:
 
 			var distance: float = self.global_position.distance_to(grapple_point)
 			if distance > rope_length:
+				var speed: float = self.velocity.length()
+				self.velocity -= self.velocity.project(self.global_position - grapple_point)
+				self.velocity = self.velocity.normalized() * speed
 				self.global_position = self.global_position.move_toward(grapple_point, distance - rope_length)
 
 			current_target.get_node("StaticBody3D").call("targeted")
